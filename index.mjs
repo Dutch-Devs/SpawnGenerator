b3a019f0dae416c6f174400c0553fe09c462f03c
Maurits Wilke <mauritswilke@gmail.com>
2022-08-22 14:27:31 +0200

    feat: :sparkles: new version uwu owo no serious commits here
    i have COMMITment issues :3

--- index.mjs
+++ index.mjs
@@ -1,18 +1,22 @@
 import fs from "fs";
 import spawns from "./spawns.json" assert { type: "json" };
+import mobs from "./mobs.json" assert { type: "json" };
 if (!fs.existsSync("./functions")) fs.mkdirSync("./functions");
 
 for (const round of Object.entries(spawns.rounds)) {
 	const commands = [];
-	const mobs = round[1];
+	const mobsToSpawn = round[1];
 
 	["north", "south", "east", "west"].forEach(v => {
 		const coords = spawns.settings[v];
 
-		mobs[v].forEach(mob => {
-			commands.push(`summon minecraft:${mob} ${coords}`);
-		})
+		for (const [nickname, amount] of Object.entries(mobsToSpawn[v])) {
+			const mobData = mobs[nickname];
+			const mobType = mobData.mobType;
+			const NBT = mobData.nbt
 
+			commands.push(`execute as @e[limit=${amount}] run summon minecraft:${mobType} ${coords} {${NBT}}`);
+		}
 	})
 
 	fs.writeFileSync(`./functions/${round[0]}.mcfunction`, commands.join("\r\n"))
